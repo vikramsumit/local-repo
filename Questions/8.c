b987a6eaa25d0430e48a1ac4d58dc859032c7aa6
Sumit Vikram <vikramsumit@outlook.com>
2025-01-12 23:16:35 +0530

    today code

---
+++ Questions/8.c
@@ -0,0 +1,14 @@
+// Write a program to print "*****" in 4 line
+
+# include <stdio.h>
+
+int main(){
+    char ch;
+    int i;
+
+    for(i = 0; i < 4;i++){
+        printf("***** \n");
+    }
+
+    return 0;
+}

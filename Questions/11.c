a61e06522e2106eaa591a42c21782e63d51f6321
Sumit Vikram <vikramsumit@outlook.com>
2025-01-13 23:40:49 +0530

    today code

--- Questions/11.c
+++ Questions/11.c
@@ -1,2 +1,22 @@
 // Write a function to find sum of digits of a number
 
+#include <stdio.h>
+int sum_two()
+{
+    float a, b, sum;
+
+    printf("Enter your first number: ");
+    scanf("%f", &a);
+    printf("Enter your second number: ");
+    scanf("%f", &b);
+
+    sum = a + b;
+
+    printf("Required sum is: %.2f\n", sum);
+}
+
+int main()
+{
+    sum_two();
+    return 0;
+}

06029ecffc6cc44808d98bb8a9e417fc3a41b4f3
Sumit Vikram <vikramsumit@outlook.com>
2025-01-14 14:36:07 +0530

    today code

---
+++ Questions/12.c
@@ -0,0 +1,18 @@
+// Write a function to find square root of a number
+
+#include<stdio.h>
+
+int sqrt(){
+    float num,a;
+
+    printf("Enter the number to find square root: ");
+    scanf("%f", &num);
+
+    a = pow(num,1/2);
+
+    printf("Square root of number is: ", a);
+}
+int main(){
+    int squrt();
+    return 0;
+}

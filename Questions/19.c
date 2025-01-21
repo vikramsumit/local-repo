1757ed5ad680d6a28e202a07738c0fc25ca73e2d
Sumit Vikram <vikramsumit@outlook.com>
2025-01-21 19:56:31 +0530

    today code

--- Questions/19.c
+++ Questions/19.c
@@ -2,20 +2,21 @@
 
 #include <stdio.h>
 
-int main() {
-    char alphabet[26]; // Array to hold the English alphabet
-    char *ptr = alphabet; // Pointer to the alphabet array
+int main()
+{
+    char alphabet[26];
+    char *ptr = alphabet;
 
-    // Fill the array with the English alphabet
-    for (int i = 0; i < 26; i++) {
-        alphabet[i] = 'A' + i; // Assign letters A to Z
+    for (int i = 0; i < 26; i++)
+    {
+        alphabet[i] = 'A' + i;
     }
 
     printf("English Alphabet using a Pointer:\n");
 
-    // Print the alphabet using the pointer
-    for (int i = 0; i < 26; i++) {
-        printf("%c ", *(ptr + i)); // Dereference the pointer to access the letter
+    for (int i = 0; i < 26; i++)
+    {
+        printf("%c ", *(ptr + i));
     }
 
     printf("\n");

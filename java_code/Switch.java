57a65c360c7e6fb1f3f441c4c136ae3e7e722bbb
Sumit Vikram <vikramsumit@outlook.com>
2025-01-23 22:20:03 +0530

    today code

---
+++ java_code/Switch.java
@@ -0,0 +1,17 @@
+public class Switch
+{
+    public static void main(String[] args) {
+        int num=100;
+
+        switch(num)
+        {
+            case 200: System.out.println("case1");
+             break;
+            case 300: System.out.println("case2");
+            break;
+            case 100: System.out.println("case3");
+            break;
+            default : System.out.println("default");
+        }
+    }   
+}

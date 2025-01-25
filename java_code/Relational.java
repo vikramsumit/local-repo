832e17ec981c45514bc0e6bacebe1138928d2d21
Sumit Vikram <vikramsumit@outlook.com>
2025-01-25 21:46:01 +0530

    today code

---
+++ java_code/Relational.java
@@ -0,0 +1,18 @@
+public class Relational 
+{
+ public static void main(String[] args) {
+    int a=10;
+    int b=7;
+    int c=20;
+
+    boolean result =a>=b;
+    boolean result2 =a!=b;
+    System.out.println(result);
+    System.out.println(result2);
+
+    //
+    System.out.println(a<b);
+    System.out.println(a<c);
+
+ }   
+}

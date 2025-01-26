5b6aaf6b3460b2929c8af9a6ee05a8f444f072ba
Sumit Vikram <vikramsumit@outlook.com>
2025-01-26 22:32:12 +0530

    today code

---
+++ java_code/question.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class question {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number:");
+        int a = sc.nextInt();
+         System.out.println("Enter Number:");
+        
+        int b = sc.nextInt();
+
+        System.out.println("Enter Number:");
+System.out.println(a+b);
+    }
+}

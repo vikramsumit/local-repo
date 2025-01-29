5297ff067e9d57e8c5fe0fa8b703a91fdf2234f7
Sumit Vikram <vikramsumit@outlook.com>
2025-01-29 12:55:33 +0530

    today code

---
+++ java_code/number.java
@@ -0,0 +1,15 @@
+//import java.util.*;
+
+public class number {
+    public static void main(String[] args) {
+        int a=444;
+        int b=55;
+
+        if(a>=b) {
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("a is smaller than b");
+        }
+    }
+}

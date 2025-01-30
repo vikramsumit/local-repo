0934516adf8696f73ee1047906ab117739f4cd75
Sumit Vikram <vikramsumit@outlook.com>
2025-01-30 21:32:41 +0530

    today code

---
+++ java_code/nono.java
@@ -0,0 +1,11 @@
+public class nono {
+ public static void main(String[] args) {
+    for(int i=0 ; i>=115; i++)
+    {
+        // System.out.print(i);
+        if (i % 2 == 1){
+            System.out.println(i);
+        }
+    }
+ }
+}

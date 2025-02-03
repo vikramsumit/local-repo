e6d41ff297502a0b7b678ab06329f5553e109c38
Sumit Vikram <vikramsumit@outlook.com>
2025-02-03 22:16:51 +0530

    today code

---
+++ java_code/Loop.java
@@ -0,0 +1,9 @@
+public class Loop 
+{
+    public static void main(String[] args) {
+        for(int i=0 ; i<5 ; i++)
+        {
+            System.out.println("This is all about loop");
+        }
+    }    
+}

52d74a5b2f298c237b1858c0ef01ec12fac04fe9
Sumit Vikram <vikramsumit@outlook.com>
2025-02-04 22:00:15 +0530

    today code

---
+++ java_code/Launch2.java
@@ -0,0 +1,16 @@
+class Launch2
+{
+
+    public static void main(String[] args) 
+    {
+        int num1=55;
+        int num2=5;
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1/num2);
+        System.out.println(500*5);
+        System.out.println(500%3);
+        System.out.println(500%7);
+        System.out.println(500%700);
+    }
+}

d99453c3c048b9200f8ae059b157a2cfe7357163
Sumit Vikram <vikramsumit@outlook.com>
2025-02-05 16:54:37 +0530

    today code

---
+++ java_code/javaBasics3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class javaBasics3 {
+    public static void main(String[] args) {
+        for( int i=1; i<=555; i++) {
+            if (i == 53) {
+                break;
+            }
+            System.out.println( i );
+        }
+
+        System.out.println(" now i am out of this loop" + " ");
+    }
+}

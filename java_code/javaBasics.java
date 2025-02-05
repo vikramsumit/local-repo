82a96105f9108b60273dfa55dff47278301072d5
Sumit Vikram <vikramsumit@outlook.com>
2025-02-05 16:55:51 +0530

    today code

---
+++ java_code/javaBasics.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class javaBasics {
+    public static void main(String[] args) {
+        int counter = 0;
+        // while(true) {
+        while(counter <= 4) {
+            System.out.println("Hello all");
+            counter++;
+    }
+        // System.out.println("print it 10x");
+
+}
+}

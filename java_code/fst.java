eb21f848e1d00d27a4aa29bf26f354f532f6a6a2
Sumit Vikram <vikramsumit@outlook.com>
2025-02-07 16:19:06 +0530

    today code

---
+++ java_code/fst.java
@@ -0,0 +1,43 @@
+//  {
+//     public static void main(string[]args)
+//     {
+//       int a=45;
+//       double b ;
+//       b=a;
+//       System.out.println(b);
+
+//       double num1=45.5;
+//       int num2;num2=(int)num1;
+//       System.out.println(num2);
+
+//       int n1=12;
+//       int n2=5;
+//       float res=n1/n2;
+//       System.out.println(res);
+
+//     }
+// }
+
+public class fst {
+  
+  public static void main(String [] args){
+    // int a=10;
+    // int b=20;
+    // int c=a+b;
+    // System.out.println(c);
+          int a=45;
+      double b ;
+      b=a;
+      System.out.println(b);
+
+      double num1=45.5;
+      int num2;num2=(int)num1;
+      System.out.println(num2);
+
+      int n1=12;
+      int n2=5;
+      float res=n1/n2;
+      System.out.println(res);
+  }
+
+}

3e67a91fdb53b7d02255bdec77a7842319d8bea1
Sumit Vikram <vikramsumit@outlook.com>
2025-02-07 16:17:26 +0530

    today code

---
+++ java_code/HelloWorld.java
@@ -0,0 +1,21 @@
+// import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // int a=5;
+        // a++;
+        // System.out.println(a);
+        // a--;
+        // System.out.println(a);
+        // ++a;
+           int a=51;
+           System.out.println(a);
+           //    int b=a++;
+           //    int b=++a;
+           // int b= a++ + ++a + --a;
+           int b= a+ + ++a + ++a + a++ + --a + a--;
+           System.out.println(b);
+           System.out.println(a);
+
+    }
+}

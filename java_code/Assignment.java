20aa10863265c9070a6c808aa53a517f121bab43
Sumit Vikram <vikramsumit@outlook.com>
2025-02-12 23:17:26 +0530

    today code

---
+++ java_code/Assignment.java
@@ -0,0 +1,26 @@
+public class Assignment 
+
+{
+    public static void main(String[] args) {
+        int m1,m2,m3,m4;
+
+        m1=m2=m3=m4=11;
+        System.out.println(m1+ " " + m2 + " " + m3 + " " + m4);
+
+        int a=12;
+        a -=5;
+        System.out.println(a);
+
+        int b=10;
+        b -=5;
+        System.out.println(b);
+
+        int c=50;
+        c /=5;
+        System.out.println(c);
+
+        int d=7;
+        d *=5;
+        System.out.println(d);
+    }
+}

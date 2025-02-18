1a0eb05e0844b0ee66818c5175f0eb4891ee9a75
Sumit Vikram <vikramsumit@outlook.com>
2025-02-18 22:05:57 +0530

    today code

---
+++ java_code/Bitmanipulation.java
@@ -0,0 +1,15 @@
+// import java.util.*;
+
+public class Bitmanipulation {
+    public static void main(String[] args) {
+        System.out.println((5 & 6));
+        System.out.println((5 | 6));
+        System.out.println((5 ^ 6));
+        System.out.println((~5));
+        System.out.println((~0));
+        System.out.println((5<<2));
+        System.out.println((55<<8));
+        System.out.println((6>>2));
+        System.out.println((6>>1));
+    }
+}

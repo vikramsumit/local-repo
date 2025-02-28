c6fa3ca616ebff54352e1829edefe6762c090c67
Sumit Vikram <vikramsumit@outlook.com>
2025-02-28 20:35:17 +0530

    first commit

---
+++ java_code/ff.java
@@ -0,0 +1,34 @@
+
+// // package myPackage;
+// import java.util.*;
+
+// class FriendClass {
+//     public static void friendFunction(MyClass obj) {
+//         System.out.println("Ff");
+//         System.out.println(" x in MyClass: " + obj.x);
+//     }
+// }
+
+// public class ff {
+//     int x = 10;
+
+//     void myMethod() {
+//         System.out.println("myMethod called");
+//         FriendClass friend = new FriendClass();
+//         friend.ff(this);
+//     }
+
+//     public static void main(String[] args) {
+//         MyClass obj = new MyClass();
+//         obj.myMethod();
+//     }
+// }
+
+// import java.util.*;
+
+public class ff {
+    public static void main(String[] args) {
+        
+    }
+    
+}

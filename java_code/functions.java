2a5766d3140dac3e5da81316e056944bcf59ac16
Sumit Vikram <vikramsumit@outlook.com>
2025-02-28 20:31:43 +0530

    first commit

---
+++ java_code/functions.java
@@ -0,0 +1,25 @@
+//import java.util.*;
+
+public class functions {
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+    public static int printmustdrinkwine(){
+        System.out.println("must drink wine");
+        System.out.println("must drink wine");
+        System.out.println("must drink wine");
+        return 4;
+    }
+    public static void main(String[] args) {
+        printHelloWorld();
+        printmustdrinkwine();
+
+        //body
+        System.out.println("hello all");
+
+        //work1
+        System.out.println("hello all");
+    }
+}

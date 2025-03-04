c8324dbfc9069d5c3b125852970a248414a7ea45
Sumit Vikram <vikramsumit@outlook.com>
2025-03-04 22:54:56 +0530

    first commit

---
+++ java_code/javaques1.java
@@ -0,0 +1,19 @@
+//import java.util.*;
+
+public class javaques1 {
+    public static void main(String[] args) {
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        for(int line=1; line<=44; line++){
+
+            for( int star=1; star<=line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("Your output will printed successfully");
+    }
+}

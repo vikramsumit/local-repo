d06b27cec073a912adb77259609f5bbbef147bdc
Sumit Vikram <vikramsumit@outlook.com>
2025-03-06 22:50:53 +0530

    first commit

---
+++ java_code/javaques4.java
@@ -0,0 +1,16 @@
+public class javaques4 {
+    public static void main(String[] args) {
+        int n=6;
+        char ch ='A';
+
+        // outer loop
+        for(int line=1; line<=n; line++){
+            //inner loop
+            for(int chars=1; chars<=line; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}

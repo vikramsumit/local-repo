089027ebc5aea2c26c8c8cb945656c78ca0e756a
Sumit Vikram <vikramsumit@outlook.com>
2025-03-10 22:46:21 +0530

    first commit

---
+++ java_code/cyclell.java
@@ -0,0 +1,124 @@
+public class cyclell {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        // create new node
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // step 2 - newNode next = head
+        newNode.next = head;
+
+        // step3 - head = newNode
+        head = newNode;
+
+    }
+
+    public void addLast(int data) {
+        // create new node
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        // assign new node next to tail
+        tail.next = newNode;
+
+        // makking last node tail
+        tail = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("ll is empty");
+            System.out.println("rom rom bhaiyo system khali hai ");
+            return;
+        }
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static boolean isCycle() { // Floyd's CFA
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+            if (slow == fast) {
+                return true; // cycle exist
+            }
+        }
+        return false; // cycle doesnot exist
+    }
+
+    public static void removeCycle() {
+        // detec₺cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+
+        // find meeting point
+        slow = head;
+        Node prev = null;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // remove cycle -> fast.prev -> null
+        prev.next = null;
+    }
+
+    public static void main(String[] args) {
+        // head = new Node(1);
+        // head.next = new Node(2);
+        // head.next.next = new Node(3);
+        // head.next.next.next = new Node(4);
+        // head.next.next.next.next = head.next;
+
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next =new Node(3);
+        head.next.next.next = temp;
+
+        System.out.println(isCycle());
+        removeCycle();
+        System.out.println(isCycle());
+    }
+}
